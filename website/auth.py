1c171638e1663452824ee45c395ae530beebf7f1
Mike Shehan <mjshehan@uivc.ca>
2024-01-19 16:01:21 -0800

    minor changes

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint 
+from flask import Blueprint, render_template, request 
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +12,17 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/calc', methods = ['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"        
+    data = None
+    if request.method == 'POST': 
+        #add checks for valid input
+        #flesh messages as needed 
+        size_1 = int(request.form.get("raw_lumber_length_1"))
+        size_2 = int(request.form.get("raw_lumber_length_2"))
+        size_3 = int(request.form.get("raw_lumber_length_3"))
+        piece_list = request.form.get("piece_list")
+        print("SUBMIT WAS DEPRESSED")
+        print(size_1, size_2, size_3, piece_list)
+    return render_template("calculator.html", text="Testing Calculator Text", boolean=True)        
+
